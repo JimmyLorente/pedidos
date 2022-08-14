f6ca381b075c597ef4e9afcaba6757a0e78c20fd
Fer :3 <lorentef12@gmail.com>
2022-08-14 11:29:15 -0500

    si

--- pedido2/pedido/urls.py
+++ pedido2/pedido/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path('crema', views.crema, name='crema' ),
     path('desayuno', views.desayuno, name='desayuno' ),
     path('almuerzo', views.almuerzo, name='almuerzo' ),
+    path('orden', views.orden, name='orden' ),
+    path('signup', views.signup, name='signup' ),
+    path('login', views.logIn, name='login' ),
+    path('logout', views.logOut, name='logout' ),
+
 ]
--- pedido2/pedido/views.py
+++ pedido2/pedido/views.py
@@ -1,7 +1,9 @@
-from ctypes.wintypes import MSG
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Almuerzo, Ensalada, Crema, Desayuno
+from .forms import NewUserForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
@@ -27,3 +29,38 @@ def almuerzo(request):
     almuerzos = Almuerzo.objects.all()
     ctx = {'almuerzos': almuerzos, 'activate_link': 'almuerzos'}
     return render(request, 'almuerzo.html', ctx)
+
+def orden(request):
+    ctx = {'activate_link': 'orden'}
+    return render(request, 'orden.html', ctx)
+
+def signup(request):
+    ctx = {}
+    if request.POST:
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+        else:
+            ctx['form'] = form
+    else:
+        form = NewUserForm()
+        ctx['form'] = form
+    return render(request, 'signup.html', ctx)
+
+def logIn(request):
+    if request.POST:
+        username = request.POST.get('username')
+        pwd= request.POST.get('password')
+        user= authenticate(request, usarname=username, password=pwd)
+        if user is not None:
+            logIn(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'usario o contraseña incorrectos')
+    ctx = {'activate_link': 'logIn'}
+    return render(request, 'login.html', ctx)
+
+def logOut(request):
+    logout(request)
+    return redirect('index')
